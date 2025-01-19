56b3c07899f88eab10590bcefd4427ca13fb0e75
Daniel Dias <diasdaniel744@gmail.com>
2025-01-19 14:28:37 -0300

    adicionei zerovalues.go

---
+++ modulo4/zerovalues.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var s string
+	var b bool
+
+	fmt.Printf("Inteiro: %v\n", i)
+	fmt.Printf("Float: %v\n", f)
+	fmt.Printf("Bool: %v\n", b)
+	fmt.Printf("String: %v\n", s)
+	
+}
