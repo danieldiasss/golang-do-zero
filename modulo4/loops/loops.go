840e017c890656c9b62f6be0175ce2b1ccc51ebd
Daniel Dias <diasdaniel744@gmail.com>
2025-03-24 23:10:16 -0300

    Update loops.go

--- modulo4/loops/loops.go
+++ modulo4/loops/loops.go
@@ -5,21 +5,21 @@ import "fmt"
 
 func main(){
 
-	// i++ -> soma 1
-	// i-- -> subtratindo 1
-	//sum := 0
-	//for i := 0; i < 10; i++ {
-		//fmt.Println("Sum", sum)
-		//fmt.Println("Indice", i)
-		//sum += i
+	 i++ -> soma 1
+	 i-- -> subtratindo 1
+	sum := 0
+	for i := 0; i < 10; i++ {
+	/fmt.Println("Sum", sum)
+		fmt.Println("Indice", i)
+		sum += i
 		// é a mesma coisa que: sum=sum + i
-		//sum -= i-> sum = sum - i
+		sum -= i-> sum = sum - i
 
 		//É como se ao ginal do loop fosse adicionado 1 ao índice i
-		// i++
-		// i = i + 1
-	//}
-	//fmt.Println(sum)
+		 i++
+		 i = i + 1
+	}
+	fmt.Println(sum)
 
 	//loop infinito 
 	//for{
